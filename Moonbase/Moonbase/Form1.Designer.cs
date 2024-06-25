469df64759b083e1acb3a83439853638e716d63c
jakeinthecrane <165937867+jakeinthecrane@users.noreply.github.com>
2024-06-24 20:17:08 -0700

    Add files via upload

---
+++ Moonbase/Moonbase/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Moonbase
+{
+    partial class MoonbaseAssignment1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MoonbaseAssignment1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelBorderMoonAndSpa = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.luxuryPackage1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minMassageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minFacialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manicurePedicureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.elegantPackage2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minMassageToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.minFacialToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.comfortPackage3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minMassageToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.refreshPackage4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minFacialToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panelBorderMoonAndSpa.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.toolStrip1);
+            this.groupBox1.Controls.Add(this.panelBorderMoonAndSpa);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1361, 314);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(515, 406);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Location Information";
+            // 
+            // panelBorderMoonAndSpa
+            // 
+            this.panelBorderMoonAndSpa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panelBorderMoonAndSpa.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelBorderMoonAndSpa.Controls.Add(this.label1);
+            this.panelBorderMoonAndSpa.Location = new System.Drawing.Point(6, 62);
+            this.panelBorderMoonAndSpa.Name = "panelBorderMoonAndSpa";
+            this.panelBorderMoonAndSpa.Size = new System.Drawing.Size(503, 52);
+            this.panelBorderMoonAndSpa.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moon Spa and Retreat";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 23);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(509, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.luxuryPackage1ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.elegantPackage2ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.comfortPackage3ToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.refreshPackage4ToolStripMenuItem,
+            this.toolStripSeparator4});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(34, 24);
+            this.toolStripDropDownButton1.Text = "Spa Treatment Options";
+            this.toolStripDropDownButton1.MouseEnter += new System.EventHandler(this.Cursor_Hand);
+            // 
+            // luxuryPackage1ToolStripMenuItem
+            // 
+            this.luxuryPackage1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minMassageToolStripMenuItem,
+            this.minFacialToolStripMenuItem,
+            this.manicurePedicureToolStripMenuItem});
+            this.luxuryPackage1ToolStripMenuItem.Name = "luxuryPackage1ToolStripMenuItem";
+            this.luxuryPackage1ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.luxuryPackage1ToolStripMenuItem.Text = "Luxury Package 1";
+            // 
+            // minMassageToolStripMenuItem
+            // 
+            this.minMassageToolStripMenuItem.Name = "minMassageToolStripMenuItem";
+            this.minMassageToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.minMassageToolStripMenuItem.Text = "90 Min Massage ";
+            // 
+            // minFacialToolStripMenuItem
+            // 
+            this.minFacialToolStripMenuItem.Name = "minFacialToolStripMenuItem";
+            this.minFacialToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.minFacialToolStripMenuItem.Text = "30 Min Facial";
+            this.minFacialToolStripMenuItem.Click += new System.EventHandler(this.minFacialToolStripMenuItem_Click);
+            // 
+            // manicurePedicureToolStripMenuItem
+            // 
+            this.manicurePedicureToolStripMenuItem.Name = "manicurePedicureToolStripMenuItem";
+            this.manicurePedicureToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.manicurePedicureToolStripMenuItem.Text = "Manicure & Pedicure";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // elegantPackage2ToolStripMenuItem
+            // 
+            this.elegantPackage2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minMassageToolStripMenuItem1,
+            this.minFacialToolStripMenuItem1});
+            this.elegantPackage2ToolStripMenuItem.Name = "elegantPackage2ToolStripMenuItem";
+            this.elegantPackage2ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.elegantPackage2ToolStripMenuItem.Text = "Elegant Package 2";
+            // 
+            // minMassageToolStripMenuItem1
+            // 
+            this.minMassageToolStripMenuItem1.Name = "minMassageToolStripMenuItem1";
+            this.minMassageToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.minMassageToolStripMenuItem1.Text = "90 Min Massage";
+            // 
+            // minFacialToolStripMenuItem1
+            // 
+            this.minFacialToolStripMenuItem1.Name = "minFacialToolStripMenuItem1";
+            this.minFacialToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.minFacialToolStripMenuItem1.Text = "30 Min Facial";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(221, 6);
+            // 
+            // comfortPackage3ToolStripMenuItem
+            // 
+            this.comfortPackage3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minMassageToolStripMenuItem2});
+            this.comfortPackage3ToolStripMenuItem.Name = "comfortPackage3ToolStripMenuItem";
+            this.comfortPackage3ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.comfortPackage3ToolStripMenuItem.Text = "Comfort Package 3";
+            // 
+            // minMassageToolStripMenuItem2
+            // 
+            this.minMassageToolStripMenuItem2.Name = "minMassageToolStripMenuItem2";
+            this.minMassageToolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.minMassageToolStripMenuItem2.Text = "90 Min Massage";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(221, 6);
+            // 
+            // refreshPackage4ToolStripMenuItem
+            // 
+            this.refreshPackage4ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minFacialToolStripMenuItem2});
+            this.refreshPackage4ToolStripMenuItem.Name = "refreshPackage4ToolStripMenuItem";
+            this.refreshPackage4ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.refreshPackage4ToolStripMenuItem.Text = "Refresh Package 4";
+            // 
+            // minFacialToolStripMenuItem2
+            // 
+            this.minFacialToolStripMenuItem2.Name = "minFacialToolStripMenuItem2";
+            this.minFacialToolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.minFacialToolStripMenuItem2.Text = "30 Min Facial ";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(221, 6);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(40, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(419, 189);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(6, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(503, 238);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(974, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 253);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click Here For A Discount Code";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // MoonbaseAssignment1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(2030, 977);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Name = "MoonbaseAssignment1";
+            this.Text = "Moonbase";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panelBorderMoonAndSpa.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem luxuryPackage1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minMassageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minFacialToolStripMenuItem;
+        private System.Windows.Forms.Panel panelBorderMoonAndSpa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem manicurePedicureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem elegantPackage2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minMassageToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem minFacialToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem comfortPackage3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minMassageToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem refreshPackage4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minFacialToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Moonbase/Moonbase/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moonbase
+{
+    public partial class MoonbaseAssignment1 : Form
+    {
+        public MoonbaseAssignment1()
+        {
+            InitializeComponent();
+        }
+
+        private void minFacialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Use the code MOONDANCE for 20% off treatment ");
+        }
+
+        private void Cursor_Hand(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
